696c2a83925bb75166b996ec32866f702b56e080
YvanPB21 <yvanpb.2000.yp@gmail.com>
2020-10-05 12:48:16 -0500

    commit prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BbComponent } from './components/bb/bb.component';
+import { AsdfComponent } from './components/asdf/asdf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BbComponent,
+    AsdfComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/bb/bb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bb',
+  templateUrl: './bb.component.html',
+  styleUrls: ['./bb.component.css']
+})
+export class BbComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
